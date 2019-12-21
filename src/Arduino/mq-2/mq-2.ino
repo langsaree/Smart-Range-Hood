4ee4a5757c8c81cfaba9a66bfb8378a0b0f705d9
N-Fah <56341873+N-Fah@users.noreply.github.com>
2019-12-21 20:30:07 +0700

    mq-2

--- src/Arduino/mq-2/mq-2.ino
+++ src/Arduino/mq-2/mq-2.ino
@@ -3,14 +3,21 @@
 #define LedRED  2  //ขา led
 #define LEDgreen  12 //ขา led
 
+int fan = 9;       // the pin where fan is
+int smokeMin = 300;   // the temperature to start the fan
+int smokeMax = 700;   // the maximum temperature when fan is at 100%
+int fanSpeed;
+
 int smokeA5; //A5
 int sensorThres = 200;  //ตั้ง ค่าความหนาของตวัน แก๊ส ก๊าซที่ 400
 
+
 void setup() {
-  Serial.begin(9600);
+  pinMode(fan, OUTPUT);
   pinMode(LedRED, OUTPUT);
   pinMode(LEDgreen, OUTPUT);
   pinMode(smokeA5, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -19,14 +26,18 @@ void loop() {
   Serial.println(analogSensor); 
 
 
-  if (analogSensor >= sensorThres)  {;
-    digitalWrite(LedRED, HIGH);
-    digitalWrite(LEDgreen, LOW);
-  }
-
-  else  {
+  if (analogSensor < 200)  {
+    fanSpeed = 0; // fan is not spinning 
+    digitalWrite(fan, LOW);
     digitalWrite(LedRED, LOW);
     digitalWrite(LEDgreen, HIGH);
   }
+
+  if ((analogSensor >= smokeMin)&& (analogSensor <= smokeMax))  {
+    fanSpeed = map(analogSensor, smokeMin, smokeMax, 32, 255); // the actual speed of fan 
+      analogWrite(fan, fanSpeed); // spin the fan at the fanSpeed spe    
+    digitalWrite(LedRED, HIGH);
+    digitalWrite(LEDgreen, LOW);
+  } 
   delay(500);
 }
